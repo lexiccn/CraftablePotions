8cf8626cce59877986b2e0444deaa838507d907a
lexiccn <13711345+lexiccn@users.noreply.github.com>
2024-05-20 15:39:38 +0100

    New PotionSettings methods

--- src/main/java/dev/lexiccn/craftablepotions/settings/PotionSettings.java
+++ src/main/java/dev/lexiccn/craftablepotions/settings/PotionSettings.java
@@ -5,10 +5,15 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 
 public class PotionSettings {
-    private static final String POTIONS = "potions.";
+    private static final String POTIONS_SETTINGS = "potions.";
+    private static final String RECIPES_SETTINGS = "recipes.";
+    private static final String TYPE = ".type";
     private static final String POTION = ".potion";
     private static final String SPLASH = ".splash";
     private static final String LINGERING = ".lingering";
+    private static final String BASE = ".base";
+    private static final String UPGRADE = ".upgrade";
+    private static final String EXTEND = ".extend";
     private static FileConfiguration config;
 
     public static void registerConfig(CraftablePotions plugin) {
@@ -32,10 +37,54 @@ public class PotionSettings {
     }
 
     private static boolean isEnabledRecipe(String id, boolean upgrade, boolean extend, String lingering) {
-        ConfigurationSection section = config.getConfigurationSection(POTIONS + id + lingering);
+        ConfigurationSection section = config.getConfigurationSection(POTIONS_SETTINGS + id + lingering);
         if (section == null) return false;
         if (upgrade) return section.getBoolean("upgrade", false);
         if (extend) return section.getBoolean("extend", false);
         return section.getBoolean("base", false);
+    }
+
+    public static boolean isEnabledQuantity(int quantity) {
+        return config.getBoolean(RECIPES_SETTINGS + "quantity." + quantity, false);
+    }
+
+    public static boolean isEnabledDynamicQuantity() {
+        return config.getBoolean(RECIPES_SETTINGS + "quantity.dynamic", false);
+    }
+
+    public static boolean revealRecipesOnJoin() {
+        return config.getBoolean(RECIPES_SETTINGS + "reveal.join", false);
+    }
+
+    public static boolean revealRecipeGroups() {
+        return config.getBoolean(RECIPES_SETTINGS + "reveal.group", false);
+    }
+
+    public static boolean groupRecipesByType() {
+        return config.getBoolean(RECIPES_SETTINGS + "group" + TYPE, false);
+    }
+
+    public static boolean groupRecipesByPotion() {
+        return config.getBoolean(RECIPES_SETTINGS + "group" + POTION, false);
+    }
+
+    public static boolean groupRecipesBySplash() {
+        return config.getBoolean(RECIPES_SETTINGS + "group" + SPLASH, false);
+    }
+
+    public static boolean groupRecipesByLingering() {
+        return config.getBoolean(RECIPES_SETTINGS + "group" + LINGERING, false);
+    }
+
+    public static boolean groupRecipesByBaseType() {
+        return config.getBoolean(RECIPES_SETTINGS + "group" + BASE, false);
+    }
+
+    public static boolean groupRecipesByUpgradeType() {
+        return config.getBoolean(RECIPES_SETTINGS + "group" + UPGRADE, false);
+    }
+
+    public static boolean groupRecipesByExtendType() {
+        return config.getBoolean(RECIPES_SETTINGS + "group" + EXTEND, false);
     }
 }
